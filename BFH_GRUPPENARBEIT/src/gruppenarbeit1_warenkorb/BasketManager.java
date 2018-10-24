5cb9657698bd973b923738844f01512f4b7c7125
michael_draeyer <micha@DESKTOP-MIHAWK.home>
2018-10-24 13:49:23 +0200

    bugfix: return after over999 message

--- BFH_GRUPPENARBEIT/src/gruppenarbeit1_warenkorb/BasketManager.java
+++ BFH_GRUPPENARBEIT/src/gruppenarbeit1_warenkorb/BasketManager.java
@@ -6,18 +6,17 @@ import java.util.Scanner;
 
 public class BasketManager {
 	/*
-	* Gruppenarbeit 01: Warenkorb
-	* Klasse 1o
-	* Ziegler, Andrin; Frei, Yannick; Dr�yer, Michael
-	*
-	*/
+	 * Gruppenarbeit 01: Warenkorb Klasse 1o Ziegler, Andrin; Frei, Yannick;
+	 * Dr�yer, Michael
+	 *
+	 */
 	private ArrayList<ShoppingItem> availableItems;
 	private ArrayList<ShoppingItem> itemsInBasket = new ArrayList<ShoppingItem>();
 	private ResourceBundle bundle;
 
 	public BasketManager(ArrayList<ShoppingItem> availableItems, ResourceBundle bundle) {
 		this.availableItems = availableItems;
-		this.bundle = bundle; 
+		this.bundle = bundle;
 	}
 
 	public void showCurrentBasket() {
@@ -25,34 +24,34 @@ public class BasketManager {
 		double totalTax = 0;
 		int itemAmountPadding = 0, itemPricePadding = 0, amountInBasket = 0;
 		String itemPrice = "", itemTotalPrice = "";
-		
+
 		System.out.println("----------------------------------------------------------------");
-		
+
 		// check if the basket is currently empty
-		if (itemsInBasket.isEmpty()) {	
+		if (itemsInBasket.isEmpty()) {
 			System.out.println(this.bundle.getString("EMPTY_BASKET"));
-		}else {
+		} else {
 			System.out.println(this.bundle.getString("YOUR_BASKET"));
 			for (ShoppingItem item : itemsInBasket) {
-			    
+
 				amountInBasket = item.getAmountInBasket();
-				itemAmountPadding = 4 - (int)(Math.log10(amountInBasket)+1);
+				itemAmountPadding = 4 - (int) (Math.log10(amountInBasket) + 1);
 				itemPricePadding = 45 - item.getName().length();
-				itemPrice = String.format("%.2f", item.getPrice()) + " CHF"; 
-				itemTotalPrice = String.format("%.2f", item.getPrice() * amountInBasket ) + " CHF" ;
-				
-				String formatedOutput = String.format(padLeft(" ", itemAmountPadding) + amountInBasket 
-														+ " " + item.getName()
- 														+ padLeft(itemPrice, itemPricePadding)
-														+ padLeft(itemTotalPrice, 15));
+				itemPrice = String.format("%.2f", item.getPrice()) + " CHF";
+				itemTotalPrice = String.format("%.2f", item.getPrice() * amountInBasket) + " CHF";
+
+				String formatedOutput = String.format(padLeft(" ", itemAmountPadding) + amountInBasket + " "
+						+ item.getName() + padLeft(itemPrice, itemPricePadding) + padLeft(itemTotalPrice, 15));
 				System.out.println(formatedOutput);
-			    
-			    totalPrice += item.getPrice() * item.getAmountInBasket();
-			    totalTax += calculateTaxOfItem(item) * item.getAmountInBasket();
+
+				totalPrice += item.getPrice() * item.getAmountInBasket();
+				totalTax += calculateTaxOfItem(item) * item.getAmountInBasket();
 			}
-			System.out.println(this.bundle.getString("TOTAL_IS") + String.format("%.2f", totalPrice) + " " + this.bundle.getString("WITH_TAX") + 
-					String.format("%.2f", totalTax) + " " + this.bundle.getString("TAXES_CHF") + String.format("%.2f",calculateTotalTax(totalPrice, totalTax)) + "%). \n");
-			
+			System.out.println(this.bundle.getString("TOTAL_IS") + String.format("%.2f", totalPrice) + " "
+					+ this.bundle.getString("WITH_TAX") + String.format("%.2f", totalTax) + " "
+					+ this.bundle.getString("TAXES_CHF")
+					+ String.format("%.2f", calculateTotalTax(totalPrice, totalTax)) + "%). \n");
+
 		}
 		showPossibleBuys();
 	}
@@ -72,90 +71,95 @@ public class BasketManager {
 			itemTax = "(" + String.format("%.2f", calculateTaxOfItem(availableItems.get(i - 1))) + " CHF)";
 
 			itemPadding = 40 - itemName.length();
-            
-            String formatedOutput = String.format(number + itemName + padLeft(itemPrice, itemPadding) + padLeft(itemTax, 15));
-            System.out.println(formatedOutput);
+
+			String formatedOutput = String
+					.format(number + itemName + padLeft(itemPrice, itemPadding) + padLeft(itemTax, 15));
+			System.out.println(formatedOutput);
 
 		}
 
 		System.out.println("---------------------\n");
-		
+
 		enableBuying();
 	}
-	
+
 	public static String padLeft(String s, int n) {
-	    return String.format("%1$" + n + "s", s);  
+		return String.format("%1$" + n + "s", s);
 	}
-	
+
 	private double calculateTaxOfItem(ShoppingItem shoppingItem) {
 		return shoppingItem.getPrice() / 100 * shoppingItem.getTaxRate();
 	}
 
 	private double calculateTotalTax(double totalPrice, double totalTax) {
-		
-		return 100* ( totalTax / ( totalPrice - totalTax ) );
+
+		return 100 * (totalTax / (totalPrice - totalTax));
 	}
-	
+
 	public void enableBuying() {
 		System.out.println(this.bundle.getString("ITEM_CHOICE"));
-		
+
 		// check for Quit
 		int productToBuy = 0;
-		while(productToBuy <= 0) {
+		while (productToBuy <= 0) {
 			productToBuy = readNumber();
 		}
 		System.out.println(this.bundle.getString("THANKS_GOT_ITEM") + productToBuy);
-	
-		ShoppingItem itemToBuy = availableItems.get(productToBuy - 1); 
-		
-		System.out.println(this.bundle.getString("HOW_MANY") + itemToBuy.getName() + " " +this.bundle.getString("CHOICE_AMOUNT_ADD"));
+
+		ShoppingItem itemToBuy = availableItems.get(productToBuy - 1);
+
+		System.out.println(this.bundle.getString("HOW_MANY") + itemToBuy.getName() + " "
+				+ this.bundle.getString("CHOICE_AMOUNT_ADD"));
 		// check for Quit
 		int amountToBuy = readAmount();
-		if(amountToBuy != 0) {
-		addItemToBasket(itemToBuy, amountToBuy);
-		}else {
+		if (amountToBuy != 0) {
+			addItemToBasket(itemToBuy, amountToBuy);
+		} else {
 			System.out.println(this.bundle.getString("CHANGED_MIND") + itemToBuy.getName());
 		}
 		showCurrentBasket();
 	}
 
 	public void addItemToBasket(ShoppingItem shoppingItem, int amount) {
-		//check amount
-		if(amount >= 1000) {
+		// check amount
+		if (amount >= 1000) {
 			showErrorAmount();
+			return;
 		}
-		
+
 		// check if basket is empty
 		if (itemsInBasket.isEmpty()) {
 			itemsInBasket.add(shoppingItem);
 			shoppingItem.setAmountInBasket(amount);
-			System.out.println(this.bundle.getString("ITEM") + shoppingItem.getName() + " " +this.bundle.getString("WAS_ADDED"));
+			System.out.println(
+					this.bundle.getString("ITEM") + shoppingItem.getName() + " " + this.bundle.getString("WAS_ADDED"));
 		} else {
 			ShoppingItem itemInBasket = getItemFromBasket(shoppingItem);
-			if( itemInBasket instanceof ShoppingItem){
-				//add amount of item in basket
-				if(itemInBasket.getAmountInBasket() + amount <= 999) {
-				itemInBasket.setAmountInBasket(itemInBasket.getAmountInBasket() + amount);
-				System.out.println(this.bundle.getString("SUCCESS_UPDATE") + shoppingItem.getName() + "!");
-				}else {
+			if (itemInBasket instanceof ShoppingItem) {
+				// add amount of item in basket
+				if (itemInBasket.getAmountInBasket() + amount <= 999) {
+					itemInBasket.setAmountInBasket(itemInBasket.getAmountInBasket() + amount);
+					System.out.println(this.bundle.getString("SUCCESS_UPDATE") + shoppingItem.getName() + "!");
+				} else {
 					showErrorAmount();
 				}
-				}else {
-				//add item to the basket with given amount
+			} else {
+				// add item to the basket with given amount
 				itemsInBasket.add(shoppingItem);
 				shoppingItem.setAmountInBasket(amount);
-				System.out.println(this.bundle.getString("ITEM") + shoppingItem.getName() + " " +this.bundle.getString("WAS_ADDED"));
+				System.out.println(this.bundle.getString("ITEM") + shoppingItem.getName() + " "
+						+ this.bundle.getString("WAS_ADDED"));
 			}
 		}
-		
+
 	}
-	
+
 	public void showErrorAmount() {
-		
+
 		System.out.println(this.bundle.getString("OVER999"));
-		
+
 	}
-	
+
 	public int readNumber() {
 		Scanner input = new Scanner(System.in);
 		int number = 0;
@@ -164,24 +168,24 @@ public class BasketManager {
 		while (number <= 0) {
 			while (!input.hasNextInt()) {
 				// check for quit
-				
-				if("q".equals(input.nextLine())) {
+
+				if ("q".equals(input.nextLine())) {
 					System.out.println(this.bundle.getString("BYE"));
 					System.exit(1);
 				}
 				System.out.println(this.bundle.getString("INVALID_INPUT"));
 				number = 0;
 			}
-			
+
 			number = input.nextInt();
-			if(number > availableItems.size()) {
+			if (number > availableItems.size()) {
 				System.out.println(this.bundle.getString("INVALID_ITEM"));
 				number = 0;
 			}
 		}
 		return number;
 	}
-	
+
 	public int readAmount() {
 		Scanner input = new Scanner(System.in);
 		int amount = 0;
@@ -190,29 +194,29 @@ public class BasketManager {
 		while (amount <= 0) {
 			while (!input.hasNextInt()) {
 				// check for quit
-				
-				if("a".equals(input.nextLine())) {
+
+				if ("a".equals(input.nextLine())) {
 					System.out.println(this.bundle.getString("ABORT"));
 				}
-				
+
 				return 0;
 			}
-			
+
 			amount = input.nextInt();
 		}
-		
+
 		return amount;
 	}
-	
+
 	public ShoppingItem getItemFromBasket(ShoppingItem item) {
-		
+
 		for (ShoppingItem shoppingItemInBasket : itemsInBasket) {
 			if (shoppingItemInBasket.getName() == item.getName()) {
 				// if the names are equal, item is already in basket
 				return shoppingItemInBasket;
-			} 
+			}
 		}
-		
+
 		return null;
 	}
 
