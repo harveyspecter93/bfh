c98021d8d7df11ba7a43c2ed79d4b14fc2afd6ff
Dräyer's Michu <mike.draeyer@gmail.com>
2020-05-23 15:54:40 +0200

    amountOfRequests

--- BFH_GRUPPENARBEIT4/src/TCPClient.java
+++ BFH_GRUPPENARBEIT4/src/TCPClient.java
@@ -3,7 +3,8 @@ import java.net.*;
 import java.io.*;
 public class TCPClient {
   public static void main(String[] args) throws Exception {
-  try{
+  int amountOfRequests = 1; //je nach Testfall anpassen
+	  try{
     Socket socket=new Socket("192.168.1.22", 8888);
     DataInputStream inStream=new DataInputStream(socket.getInputStream());
     DataOutputStream outStream=new DataOutputStream(socket.getOutputStream());
@@ -14,7 +15,7 @@ public class TCPClient {
       System.out.println("Enter a Number, \"Ping\" or \"Bye\"");
       clientMessage=br.readLine();
       
-      for (int i = 0; i < 1; i++) {
+      for (int i = 0; i < amountOfRequests; i++) { 
     	  outStream.writeUTF(clientMessage);
           outStream.flush();
           serverMessage=inStream.readUTF();
